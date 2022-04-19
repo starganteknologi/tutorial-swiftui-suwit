997cb2f418f3b50d26798b63eaab70d7b9e2f384
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-19 20:37:00 +0100

    init

---
+++ suwit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  suwit
+//
+//  Created by fathur rohman on 19/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ suwit/suwitApp.swift
@@ -0,0 +1,17 @@
+//
+//  suwitApp.swift
+//  suwit
+//
+//  Created by fathur rohman on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct suwitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
